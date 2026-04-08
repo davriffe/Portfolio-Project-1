9fca208391d9fbee96c5325c67327b3187b08e29
David Riffey <davriffe@gmail.com>
2026-04-08 09:46:53 -0500

    finished the createagent section in the agent.js

--- js/simulation/agent.js
+++ js/simulation/agent.js
@@ -217,5 +217,46 @@ function createAgent(archetypeId) {
     const isFirstVisit = Math.random() < 0.30;
     const isForeignVisitor = Math.random() < 0.15;
 
-    
+    const stayHours = template.baseStats.stayHours.min + Math.random() * (template.baseStats.stayHours.Max - template.baseStats.stayHours.min);
+
+    const agent = {
+        id: crypto.randomUUID(),
+        archetypeId: archetypeId,
+        name: template.name,
+        fixed: { ...template.fixed },
+        modifiers: {
+            isFirstVisit: isFirstVisit,
+            isForeignVisitor: isForeignVisitor
+        },
+        dynamic: {
+            energy: 100,
+            satisfaction: 100,
+            balkingMinutes: { ...template.baseStats.balkingMinutes },
+            stayMinutesRemaining: stayHours * 60,
+            currentLand: "crossroads",
+            currentAttraction: null,
+            waitingMinutes: 0,
+            cooldowns: {}
+        }
+    };
+
+    if (isFirstVisit) {
+        agent.dynamic.balkingMinutes.preferred *= 0.80;
+        agent.dynamic.balkingMinutes.other *= 0.80;
+    }
+
+    if (isForeignVisitor) {
+        agent.dynamic.balkingMinutes.preferred *=1.25;
+        agent.dynamic.balkingMinutes.other *= 1.25;
+        agent.fixed.preferenceWeights.activities += 0.10;
+        agent.fixed.preferenceWeights.shopping += 0.15;
+        agent.fixed.preferenceWeights.attractions -= 0.25;
+    }
+
+    if (archetypeId === "ride_activity_enthusiast") {
+        const flavor = Math.random() < 0.50 ? "ride" : "activity";
+        agent.fixed.flavor = flavor;
+        agent.fixed.preferenceWeights = { ...template.fixed.flavors[flavor] };
+    }
+    return agent;
 }
